d171c9f4a26800aeaf793cbf50f6a38afc42e5c7
bitnami <mc3683@columbia.edu>
2013-12-04 17:34:50 -0500

    Added map

---
+++ js/app.js
@@ -0,0 +1,1 @@
+angular.module('maptesting', ['ui.map','ui.event']);
---
+++ js/controllers.js
@@ -0,0 +1,23 @@
+function MapCtrl($scope) {
+    var ll = new google.maps.LatLng(13.0810, 80.2740);
+    $scope.mapOptions = {
+        center: ll,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //Markers should be added after map is loaded
+    $scope.onMapIdle = function() {
+        if ($scope.myMarkers === undefined){    
+            var marker = new google.maps.Marker({
+                map: $scope.myMap,
+                position: ll
+            });
+            $scope.myMarkers = [marker, ];
+        }
+    };
+
+    $scope.markerClicked = function(m) {
+        window.alert("clicked");
+    };
+}
