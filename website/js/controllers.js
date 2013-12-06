00ad955918526b615e3c42cc1c743a963a13a676
TheFifthFreedom <laurent.mazouer@gmail.com>
2013-12-05 22:56:22 -0500

    Revert 35e093c..64ba5d8
    This rolls back to commit 35e093c0ba417b999f7ffcb6d1047961c841af66.

--- website/js/controllers.js
+++ website/js/controllers.js
@@ -1,60 +1,57 @@
-app.controller('MyCtrl', function($scope, FoursquareService) {
-    //40.78,-73.97 -> New York
+app.controller('MapCtrl', function ($scope) {
     var ll = new google.maps.LatLng(40.78,-73.97);
-    //$scope.myMarkers = [];
     $scope.mapOptions = {
-    center: ll,
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+        center: ll,
+        disableDefaultUI: true,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     //Markers should be added after map is loaded
     $scope.onMapIdle = function() {
-        if ($scope.myMarkers === undefined){
+        if ($scope.myMarkers === undefined){    
             var marker = new google.maps.Marker({
-            map: $scope.myMap,
-            position: ll
-        });
-        $scope.myMarkers = [marker, ];
+                map: $scope.myMap,
+                position: ll
+            });
+            $scope.myMarkers = [marker, ];
         }
     };
 
     $scope.markerClicked = function(m) {
         window.alert("clicked");
-        var marker = new google.maps.Marker({
-            map: $scope.myMap,
-            position: new google.maps.LatLng(40.78,-73.98)
-        });
-        $scope.myMarkers.push(marker);
     };
+});
 
-    $scope.searchFoursquare = function (searchItem){ 
-        FoursquareService.get({ll:searchItem},function(reply){
-        $scope.venues = reply.response.venues;
-        //console.log(reply);
-    });};
+app.controller('MyCtrl', function($scope, FoursquareService, FileSystemService) {
 
     var txtFileName = "data.txt";
     $scope.messages = ['Click a button'];
-    venues_list = "";
-    // 40.78,-73.97 -> New York
-    // 42.3581,71.0636 -> Boston
+
+    //40.78,-73.97 -> New York
 
     $scope.searchFoursquare = function (searchItem){ 
         FoursquareService.get({ll:searchItem},function(reply){
         $scope.venues = reply.response.venues;
         console.log(reply);
-        for(var i=0; i<reply.response.venues.length; i++){
-            venues_list += reply.response.venues[i].name+"\n";
-        }
     });};
 
-    $scope.writeVal =   function() {
-                            store.set( "whatever",venues_list );
-                        };
 
-    $scope.readVal =    function() {
-                            console.log( store.get( "whatever" ) );
-                        };
-
+    $scope.writeVal = function() {
+        FileSystemService.writeText(txtFileName, "Persistence TEST!!").then(function(fs) {
+            $scope.messages.push("data written");
+        }, function(err) {
+            console.log(err);
+            $window.alert(err.text);
+        });
+    };
+    
+    $scope.readVal = function() {
+        FileSystemService.readFile(txtFileName).then(function(contents) {
+            $scope.messages.push(contents);
+        }, function(err) {
+            console.log(err);
+            $window.alert(err.text);
+        });
+    };
 });
